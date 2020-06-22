5841316b2cea142a862b4c78fe461e5929c5d519
Vlad <fordw0w@yandex.ru>
2020-06-22 16:39:52 +0300

    printJava

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void printJava() {
+        String str1 = "Хорошо идут дела";
+        System.out.println(str1);
+        String str2 = "Изучаю Java я!";
+        System.out.println(str2);
+        String str3 = " ";
+        System.out.println(str1 + str3 + str2);
+        System.out.println(str2 + str3 + str1);
+
+    }
+    public static void main(String[] args) {
+
+        printJava();
+    }
+}
