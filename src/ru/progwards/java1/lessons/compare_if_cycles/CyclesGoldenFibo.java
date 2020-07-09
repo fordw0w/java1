e6ac90520bc039c46d287e2408ba7b3ce5d8b58e
Vlad <fordw0w@yandex.ru>
2020-07-09 19:26:36 +0300

    Test1

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -7,72 +7,21 @@ public class CyclesGoldenFibo {
             return true;
         int q1 = 10;
         int q2= 10;
-        int q3 = number % 10;
 
  if ( number >= 0) {
         while (q1 <= number) {
             int q = (q1 * q2);
-            int ww = number/q;
+            int ww = number/q; // первое число (0)
             int ww2 = ww%10;
             q1 = q;
             int hh = number%100; // 34
             int hhh = hh%10; // последнее число
             int hhhh= hh/10; // предпоследнее число
-//            System.out.println(q);
-//            первое число  ( тут 0)
-//            System.out.println(ww);
-//            System.out.println(ww3);
-//            System.out.println(hhh); последнее число
-//            System.out.println(hhhh); предпоследнее
             if (ww > 0 & digit == ww2 | digit == hhh | digit == hhhh)
                 return true;
         }
         }
         return false;
-
-//// МНЕ ЗА ЭТУ РАЗРАБОТКУ ТАКУЮ ПРЕМИЮ ДАДУУТ..
-//
-//        int a1 = number % 10;
-//        int a2 = number / 10;
-//        int a3 = a2 % 10;
-//        int a4 = number / 100;
-//        int a5 = number / 1000;
-//        int a6 = a4 % 10;
-//        int a7 = number / 10000;
-//        int a8 = a5 % 10;
-//        int a9 = number / 100000;
-//        int a10 = a7 % 10;
-//        int a11 = number / 1000000;
-//        int a12 = a9 % 10;
-//        int a13 = number / 10000000;
-//        int a14 = a11 % 10;
-//        int a15 = number / 100000000;
-//        int a16 = a13 % 10;
-//        int a17 = number / 1000000000;
-//        int a18 = a15 % 10;
-//
-//        if (number <= 9 && number == digit)
-//            return true;
-//        else if ((number <= 99) && (a1 == digit) | (a2 == digit))
-//            return true;
-//        else if ((number <= 999) && (a4 == digit) | (a2 == digit) | (a3 == digit) | (a1 == digit))
-//            return true;
-//        else if ((number <= 9999) && (a4 == digit) | (a2 == digit) | (a3 == digit) | (a1 == digit) | (a5 == digit) | (a6 == digit))
-//            return true;
-//        else if ((number <= 99999) && (a4 == digit) | (a2 == digit) | (a3 == digit) | (a1 == digit) | (a5 == digit) | (a6 == digit) | (a7 == digit) | (a8 == digit))
-//            return true;
-//        else if ((number <= 999999) && (a4 == digit) | (a2 == digit) | (a3 == digit) | (a1 == digit) | (a5 == digit) | (a6 == digit) | (a7 == digit) | (a8 == digit) | (a9 == digit) | (a10 == digit))
-//            return true;
-//        else if ((number <= 9999999) && (a4 == digit) | (a2 == digit) | (a3 == digit) | (a1 == digit) | (a5 == digit) | (a6 == digit) | (a7 == digit) | (a8 == digit) | (a9 == digit) | (a10 == digit) | (a11 == digit) | (a12 == digit))
-//            return true;
-//        else if ((number <= 99999999) && (a4 == digit) | (a2 == digit) | (a3 == digit) | (a1 == digit) | (a5 == digit) | (a6 == digit) | (a7 == digit) | (a8 == digit) | (a9 == digit) | (a10 == digit) | (a11 == digit) | (a12 == digit) | (a13 == digit) | (a14 == digit))
-//            return true;
-//        else if ((number <= 999999999) && (a4 == digit) | (a2 == digit) | (a3 == digit) | (a1 == digit) | (a5 == digit) | (a6 == digit) | (a7 == digit) | (a8 == digit) | (a9 == digit) | (a10 == digit) | (a11 == digit) | (a12 == digit) | (a13 == digit) | (a14 == digit) | (a15 == digit) | (a16 == digit))
-//            return true;
-//        else if ((number <= 2147483646) && (a4 == digit) | (a2 == digit) | (a3 == digit) | (a1 == digit) | (a5 == digit) | (a6 == digit) | (a7 == digit) | (a8 == digit) | (a9 == digit) | (a10 == digit) | (a11 == digit) | (a12 == digit) | (a13 == digit) | (a14 == digit) | (a15 == digit) | (a16 == digit) | (a17 == digit) | (a18 == digit))
-//            return true;
-//        else
-//        return false;
     }
 
     public static int fiboNumber(int n) {
@@ -84,14 +33,6 @@ public class CyclesGoldenFibo {
         int a2 = 1;
         int a3 = 0;
         for (int i = 3; i <= n; i++) {
-//            int n1 = a1 + a2;
-//            int n2 = n1 + a2;
-//            int n3 = n2 + n1;
-//            int n4 = n2 + n3; // как будет вести счет цикл
-//            int n5 = n3 + n4;
-//            int n6 = n4 + n5;
-//            int n7 = n5 + n6;
-//            int n8 = n6 + n7;
             a3 = a1 + a2; // формула по которой считает цикл
             a1 = a2; // значения который будут меняться и подставляться в формулу
             a2 = a3;
@@ -103,9 +44,6 @@ public class CyclesGoldenFibo {
     //  которая будет возвращать true, если треугольник со сторонами a, b, c является Золотым
 // Золотой треугольник это равнобедренный треугольник
     public static boolean isGoldenTriangle(int a, int b, int c) {
-
-//        if (TriangleInfo.isIsoscelesTriangle(a, b, c)) {
-//            System.out.println("Треугольник равнобедренный");
           if (a == b) {
             System.out.println(" a = b значит ребро " + a + " и " + b + " следовательно с - основание = " + c);
             int rebro = a;
@@ -115,109 +53,51 @@ public class CyclesGoldenFibo {
 //            float osnovanieC = fosn / frebro;
               float osnovanieC = frebro / fosn;
 
-            System.out.println(" делим основание на ребро получаем " + osnovanieC);
+            System.out.println(" делим ребро на основание получаем " + osnovanieC);
             if (osnovanieC >= 1.61703 && osnovanieC < 1.61903)
                 return true;
         } else if (a == c) {
-            System.out.println(" a = c значит ребро " + a + " " + c + "значит с - основание = " + b);
+            System.out.println(" a = c значит ребро " + a + " и " + c + " следовательно с - основание = " + b);
             int rebro1 = a;
             int osn1 = b;
             float frebro1 = (float) rebro1;
             float fosn1 = (float) osn1;
 //            float osnovanieC1 = fosn1 / frebro1;
               float osnovanieC1 = frebro1 / fosn1;
-            System.out.println(" делим основание на ребро получаем " + osnovanieC1);
+            System.out.println(" делим ребро на основание получаем " + osnovanieC1);
             if (osnovanieC1 >= 1.61703 && osnovanieC1 < 1.61903)
                 return true;
         } else if (b == c) {
-            System.out.println(" b = c значит ребро " + b + " " + c + "значит с - основание = " + a);
+            System.out.println(" b = c значит ребро " + b + " и " + c + " следовательно с - основание = " + a);
             int rebro2 = b;
             int osn2 = a;
             float frebro2 = (float) rebro2;
             float fosn2 = (float) osn2;
 //            float osnovanieC2 = fosn2 / frebro2;
             float osnovanieC2 = frebro2 / fosn2;
-            System.out.println(" делим основание на ребро получаем " + osnovanieC2);
+            System.out.println(" делим ребро на основание получаем " + osnovanieC2);
             if (osnovanieC2 >= 1.61703 && osnovanieC2 < 1.61903)
                 return true;
         } else ;
         return false;
     }
 
-//            int max = TriangleSimpleInfo.maxSide(a, b, c);
-//            int min = TriangleSimpleInfo.minSide(a, b, c);
-//            float f1 = (float) max;
-//            float f2 = (float) min;
-//            float reb = f2 / f1;
-//            int rebi = (int) reb;
-//            System.out.println(TriangleSimpleInfo.maxSide(a, b, c) + " - Основание");
-//            System.out.println(TriangleSimpleInfo.minSide(a, b, c) + " - Ребро");
-//            System.out.println("Золотое сечение этого треугольника в float значении = " + reb);
-//            System.out.println("Золотое сечение этого треугольника в int значении = " + rebi);
-//         if (reb >= 1.61703 && reb <1.61903) {
-//             return true;
-//         } else
-//             return false;
-//
-//    }
-
 
 
     public static void main(String[] args) {
-//        System.out.println(containsDigit(1234, 4));
-//        System.out.println(isGoldenTriangle(233, 377, 377));
-        for (int i = 3; i <= 15; i++) {
+        for (int i = 2; i <= 15; i++) {
             System.out.println(fiboNumber(i));
         }
+        int a1 = 0;
+        int a2 = 0;
+        for (int i = 1; i <= 10; i++) {
+    a1 = fiboNumber(i);
+    a2 = fiboNumber(++i);
+    if (isGoldenTriangle(a2,a2,a1))
+        System.out.println(isGoldenTriangle(a2,a2,a1));
 
-        System.out.println(isGoldenTriangle(2,3,3));
-        System.out.println(isGoldenTriangle(3,3,5));
-        System.out.println(isGoldenTriangle(5,5,8));
-        System.out.println(isGoldenTriangle(8,8,13));
-        System.out.println(isGoldenTriangle(13,13,21));
-        System.out.println(isGoldenTriangle(21,21,34));
-        System.out.println(isGoldenTriangle(34,34,55));
-        System.out.println(isGoldenTriangle(55,55,89));
-        System.out.println(isGoldenTriangle(55,55,34));
-//        int number= 12364364;
-//        int digit = 2;
-//        int q1 = 10;
-//        int q2= 10;
-//        int q3 = number % 10;
-//
-//
-//       while (q1 <= 999999999) {
-//
-//           int q = (q1 * q2);
-//           int ww = number/q;
-//           int ww2 = ww%10;
-//           int ww3 = ww%100;
-//            q1=q;
-//        int hh = number%100; // 34
-//        int hhh = hh%10; // 4
-//        int hhhh= hh/10;
-//           System.out.println(ww2);
-//if (digit == ww2 | digit == hh | digit == hhhh)
-//           System.out.println("есть " + ww2);
-////
-////
-////
-//        }
-//        int number = 1234;
-//        System.out.println(1234%100);
-//        int hh = number%100; // 34
-//        int hhh = hh%10; // 4
-//        int hhhh= hh/10;
-//        System.out.println(number);
-//        System.out.println(hh);
-//        System.out.println(hhh);
-//        System.out.println(hhhh);
-//        System.out.println(isGoldenTriangle(3, 3, 5));
-//        System.out.println(isGoldenTriangle(8, 13, 8));
-//        System.out.println(isGoldenTriangle(34, 55, 34));
-//        System.out.println(containsDigit(5621, 2));
-
-
+//    System.out.println(isGoldenTriangle(a2,a2,a1));
+}
     }
 }
 
--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -5,25 +5,13 @@ import org.w3c.dom.ls.LSOutput;
 public class TriangleInfo {
     //    которая возвращает true, если по данным трём сторонам (a, b, c) можно построить треугольник.
 //    Из геометрии известно, что в треугольнике длина каждой из сторон меньше суммы длин двух других сторон.
-//     (c < a +b)
+
     public static boolean isTriangle(int a, int b, int c) {
         if ((a < b + c) & (b < a + c) & (c < a + b))
             return true;
         return false;
     }
 
-    //    которая возвращает true, если треугольник со сторонами a, b, c является прямоугольным.
-//    Из геометрии известно, что для прямоугольного треугольника выполняется теорема
-//    Пифагора(сумма квадратов катетов равна квадрату гипотенузы).
-//    c^2 = a^2 + b^2
-    //  ac & ab - kateti; bc - gipotenuza
-    // bc^2 -  ac^2 + ab^2
-//    a2= b2 +c2
-//    например а - большая сторона, в и с - две другие
-//    найти а^2 и b^2+c^2
-//    c * c = a * a + b * b
-//    a * a = c * c + b * b
-//    b * b = c * c + a * a
     public static boolean isRightTriangle(int a, int b, int c) {
 
         if (((c * c) == (a * a) + (b * b)) || ((a * a) == (c * c) + (b * b)) || ((b * b) == (c * c) + (a * a)))
@@ -31,27 +19,6 @@ public class TriangleInfo {
         else
             return false;
     }
-    // 2 версия
-//        int max = TriangleSimpleInfo.maxSide(a, b, c);
-//        System.out.println("Максимальное значение " +max);
-//        int min = TriangleSimpleInfo.minSide(a, b, c);
-//        System.out.println("Минимальное значение " +min);
-//
-//        int qq = (max * max);
-//        int qq2 = (min * min) + (min * min);
-//        if (qq == qq2)
-//            return true;
-//        else return false;
-//    }
-
-    //  1 версия
-//        if (((a * a) + (b * b) == (c * c)) & ((c * c) + (b * b) == (a * a)) & ((c * c) + (a * a) == (b * b))) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-
-
 
 //    которая возвращает true, если треугольник со сторонами a, b, c является равнобедренным.
 //    Из геометрии известно, что в равнобедренном треугольнике есть две равные стороны.
@@ -61,9 +28,7 @@ public class TriangleInfo {
         return false;
 
     }
-
     public static void main(String[] args) {
-
         System.out.println(isRightTriangle(21, 34, 39));
     }
 }
